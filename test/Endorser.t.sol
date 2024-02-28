a13d4b5cc4bd2b5c7c18ce819808224b7123bf0e
Agusx1211 <agusgit@pm.me>
2024-02-28 21:38:41 +0000

    Initial commit

---
+++ test/Endorser.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Endorser} from "../src/Endorser.sol";
+
+contract EndorserTest is Test {
+  
+}
